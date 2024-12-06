72152951910f55c10000d69735a9ff4faa3f17b1
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-12-06 14:07:34 +0000

    2 files read, process heavy

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const fs=require('fs')
-const file=fs.readFileSync('a.txt','utf-8',(err,data)=>{
+const file1=fs.readFileSync('a.txt','utf-8',(err,data)=>{
   console.log(data)
 })
-console.log(file)
+console.log(file1)
+const file2=fs.readFileSync('b.txt','utf-8',(err,data)=>{
+  console.log(data)
+})
+console.log(file2)
